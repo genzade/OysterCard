332f5238e908d5be4c5ddceaca51bcd01e41f374
Hamid Zade <hamidmamadzade@gmail.com>
2015-12-27 14:28:47 +0000

    README with user stories added

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,5 +1,11 @@
-
+require 'oyster_card'
 
 describe OysterCard do
-  
+  subject(:oyster_card) { described_class.new }
+
+  describe '#balance' do
+    it 'has a default balance of 0' do
+      expect(oyster_card.balance).to eq OysterCard::DEFAULT_BALANCE
+    end
+  end
 end
