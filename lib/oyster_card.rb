a67afd696729dddb26fae51f9216972743256de1
Hamid Zade <hamidmamadzade@gmail.com>
2015-12-28 16:36:09 +0000

    "touch in" "touch out" implemented (and tested) and refactor

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -2,14 +2,31 @@ class OysterCard
   DEFAULT_BALANCE = 0
   LIMIT = 90
 
-  attr_reader :balance
+  attr_reader :balance, :travelling
 
   def initialize
     @balance = DEFAULT_BALANCE
+    @travelling = false
   end
 
   def top_up(amount)
+    raise 'Maximum Limit: £90' if amount + balance > LIMIT
     @balance += amount
-    raise 'Maximum Limit: £90' if @balance > LIMIT
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    @travelling = true
+  end
+
+  def touch_out
+    @travelling = false
+  end
+
+  def in_journey?
+    !!@travelling
   end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -3,9 +3,17 @@ require 'oyster_card'
 describe OysterCard do
   subject(:oyster_card) { described_class.new }
 
-  describe '#balance' do
-    it 'has a default balance of 0' do
-      expect(oyster_card.balance).to eq OysterCard::DEFAULT_BALANCE
+  describe 'initialization' do
+    describe '#balance' do
+      it 'has a default balance of 0' do
+        expect(oyster_card.balance).to eq OysterCard::DEFAULT_BALANCE
+      end
+    end
+
+    describe '#travelling' do
+      it 'is initially not in a journey' do
+        expect(oyster_card).not_to be_in_journey
+      end
     end
   end
 
@@ -20,4 +28,37 @@ describe OysterCard do
       expect { oyster_card.top_up(1) }.to raise_error "Maximum Limit: £#{maximum_balance}"
     end
   end
+
+  describe '#deduct' do
+    it 'deducts amount from the oyster card' do
+      maximum_balance = OysterCard::LIMIT
+      oyster_card.top_up(maximum_balance)
+      expect { oyster_card.deduct(2) }.to change { oyster_card.balance }.by -2
+    end
+  end
+
+  context 'in journey' do
+    before do
+      oyster_card.top_up(20)
+      oyster_card.touch_in
+    end
+
+    describe '#touch_in' do
+      it "should be in journey when touched in" do
+        expect(oyster_card).to be_in_journey
+      end
+    end
+
+    describe '#touch_out' do
+      it 'not be in journey when touched out' do
+        oyster_card.touch_out
+        expect(oyster_card).not_to be_in_journey
+      end
+    end
+  end
 end
+
+
+
+
+
